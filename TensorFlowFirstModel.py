03d022cb4258dbbe441a433fd1c7486f8becd605
Mayolov <danyvalencia25@gmail.com>
2023-01-23 20:56:27 -0800

    space

---
+++ TensorFlowFirstModel.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train, x_test = x_train/ 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128, activation= 'relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10)  
+    ])
+
+predictions =  model(x_train[:1]).numpy()
+print(predictions)
+
+probabilites = tf.nn.softmax(predictions).numpy()
+
+print(probabilites)
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+initial_loss = loss_fn(y_train[:1], predictions).numpy()
+
+print(initial_loss)
+
+#if you want the model to return a probability, you can wrap the train model, and attach softmax to it
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+
+probability_model(x_test[:5])
+
+model.compile(optimizer = 'adam', loss =loss_fn, metrics=['accuracy'])
+model.fit(x_train,y_train,epochs = 5)
+
+model.evaluate(x_test,y_test,verbose = 2)
